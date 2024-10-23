68fd2a1243615f4ac46e578e230cab11f708353f
khatija <shaikhkhatijabi03@gmail.com>
2024-10-23 22:36:07 +0530

    day-13

--- DAY-14/index.js
+++ DAY-14/index.js
@@ -1,1 +1,37 @@
-// .////demo
+//Object
+
+// var myObject={key: data, key2:data2} syntax
+
+// var myData={
+//     name:"Khatija",
+//     age:"20",
+//     designation:"Web Developer",
+//     isopen:true
+// }
+// console.log(myData.name)
+
+// var myInstitute=new Object()       //can be assign through . and []
+// myInstitute.name="Awdiz";
+// myInstitute.age=20;
+// myInstitute["Students count"]=70;
+// myInstitute["isopen"]=true;
+// console.log(myInstitute)
+
+//re-assign
+var myInstitute={name:"Demo",isopen:false,age:10}
+// myInstitute.name="Awdiz";
+// myInstitute.age=30;
+// myInstitute["Students count"]=70;
+// myInstitute["isopen"]=true;
+// console.log(myInstitute)
+// console.log("age" in myInstitute)
+// console.log("factor" in myInstitute)
+
+//delete
+// delete myInstitute.age
+// console.log(myInstitute)
+
+
+for(var keys in myInstitute){
+    console.log(keys,myInstitute[keys])
+}
