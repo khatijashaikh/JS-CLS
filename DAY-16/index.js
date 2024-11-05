da66465a39ff4afbbc10ddab7ed06d07a3540562
khatija <shaikhkhatijabi03@gmail.com>
2024-11-05 09:16:49 +0530

    16

---
+++ DAY-16/index.js
@@ -0,0 +1,28 @@
+//fetch;event loop
+
+// localstorage
+// localStorage.setItem("MyNumber",{count:10})
+
+
+
+
+//Set Timeout           setTimeout(function,time)
+// example:
+// setTimeout(()=>{
+//     console.log("Hii");
+// } , 3000)
+
+// setInterval(()=>{
+//     console.log("Hi fromInterval")
+// },3000)
+// clearInterval()
+
+// let count=0
+// const countid= setInterval(()=>{
+//     count++
+//     console.log(count)
+//     if(count>5){
+//         clearInterval(countid)
+//         console.log("Interval Cleared")
+//     }
+// },2000)
